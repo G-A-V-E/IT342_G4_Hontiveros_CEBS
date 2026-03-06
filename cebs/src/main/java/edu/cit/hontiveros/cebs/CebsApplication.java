d56e5f0a47ddbbd8445f20fa58ec8cf8dd10d851
LAB24_USER ON WS33 <L24X09W33@LabHCI.net>
2026-03-06 08:35:17 +0800

    New File Creation

---
+++ cebs/src/main/java/edu/cit/hontiveros/cebs/CebsApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.hontiveros.cebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebsApplication.class, args);
+	}
+
+}
---
+++ cebs/src/test/java/edu/cit/hontiveros/cebs/CebsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.hontiveros.cebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CebsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
